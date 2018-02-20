f26cba07c2517540904c578d2c37669889bfde7e
NishidaYuuhei <yuhei_nishida@kyoso.co.jp>
2018-02-20 14:29:12 +0900

    initial import

---
+++ ma.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+ class Ma < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/NishidaYuhei/ma-mi"
+  sha256 "cda691f8b28378a8a457843ef0434614af90ee0e0ca9e9c0546ddaa5ddb660c7"
+   # depends_on "cmake" => :build
+   def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+     # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+   test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test ma`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
